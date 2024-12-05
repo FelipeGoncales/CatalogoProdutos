24c3cd1441151f917cf0df615047fceb1c6148f3
FelipeGoncales <felipeggoncales22@gmail.com>
2024-12-05 11:00:06 -0300

    gambiarra preço

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,103 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let catalogoLocalStorage = localStorage.getItem('catalogo');
+    let catalogoLocalStorage = localStorage.getItem('catalogo');
 
-  if (catalogoLocalStorage) {
-    let catalogoDesc = JSON.parse(catalogoLocalStorage);
+    if (catalogoLocalStorage) {
+        let catalogoDesc = JSON.parse(catalogoLocalStorage);
 
-    catalogo = catalogoDesc;
+        catalogo = catalogoDesc;
 
-    for (item of catalogoDesc) {
-      addProduto(item.nome, item.image, item.categoria, item.preco);
+        for (item of catalogoDesc) {
+            addProduto(item.nome, item.image, item.categoria, item.preco);
+        }
     }
-  }
 });
 
 let catalogo = [];
 
 const adicionarCadastro = document.getElementById("adicionarCadastro");
 adicionarCadastro.addEventListener('click', () => {
-  let nome = document.getElementById('nome').value;
-  let image = document.getElementById('image').value;
-  let categoria = document.getElementById('categoria').value;
-  let preco = document.getElementById('preco').value;
+    let nome = document.getElementById('nome').value;
+    let image = document.getElementById('image').value;
+    let categoria = document.getElementById('categoria').value;
+    let preco = document.getElementById('preco').value;
 
-  let item = {};
-  item['nome'] = nome;
-  item['image'] = image;
-  item['categoria'] = categoria;
-  item['preco'] = preco;
+    let item = {};
+    item['nome'] = nome;
+    item['image'] = image;
+    item['categoria'] = categoria;
+    item['preco'] = preco;
 
-  catalogo.push(item);
+    catalogo.push(item);
 
-  console.log(item);
-  console.log(catalogo);
+    console.log(item);
+    console.log(catalogo);
 
-  modalCadastro.style.display = "none";
+    modalCadastro.style.display = "none";
 
-  addProduto(nome, image, categoria, preco);
+    addProduto(nome, image, categoria, preco);
 
-  salvarLocalStorage();
-});
+    salvarLocalStorage();
+    });
 
-function salvarLocalStorage() {
-  let catalogoJSON = JSON.stringify(catalogo);
+    function salvarLocalStorage() {
+    let catalogoJSON = JSON.stringify(catalogo);
 
-  localStorage.setItem('catalogo', catalogoJSON);
+    localStorage.setItem('catalogo', catalogoJSON);
+    }
+
+    function addProduto(nome, image, categ, preco) {
+    let divProd = document.createElement('div');
+    divProd.classList.add('prod');
+
+    let divImage = document.createElement('div');
+    divImage.style.backgroundImage = `url(${image})`;
+    divImage.classList.add('div-img');
+    divProd.appendChild(divImage);
+
+    let divTextos = document.createElement('div');
+    divTextos.classList.add('div-textos');
+
+    let pDescricao = document.createElement('p');
+    pDescricao.classList.add('descricao');
+    pDescricao.textContent = nome;
+    divTextos.appendChild(pDescricao);
+
+    let pPreco = document.createElement('p');
+    pPreco.classList.add('preco');
+
+    let valorInteiro = parseInt(preco);
+
+    let centavos = parseInt(((preco - valorInteiro)*100));
+
+    pPreco.textContent = 'R$'+valorInteiro+','+centavos;
+    divTextos.appendChild(pPreco);
+
+    let pCategoria = document.createElement('p');
+    pCategoria.classList.add('categoria');
+    pCategoria.textContent = categ;
+    divTextos.appendChild(pCategoria);
+
+    divProd.appendChild(divTextos);
+
+    let divIcons = document.createElement('div');
+    divIcons.classList.add('div-icons');
+
+    let iconEdit = document.createElement('i');
+    iconEdit.classList.add('fa-solid', 'fa-pen', 'edit-button');
+    divIcons.appendChild(iconEdit);
+
+    let iconDelete = document.createElement('i');
+    iconDelete.classList.add('fa-solid', 'fa-trash', 'delete-button');
+    divIcons.appendChild(iconDelete);
+
+    divProd.appendChild(divIcons);
+
+    document.getElementById('produtosList').appendChild(divProd);
 }
 
-function addProduto(nome, image, categ, preco) {
-  let divProd = document.createElement('div');
-  divProd.classList.add('prod');
-  
-  let divImage = document.createElement('div');
-  divImage.style.backgroundImage = `url(${image})`;
-  divImage.classList.add('div-img');
-  divProd.appendChild(divImage);
+function deletar() {
 
-  let divTextos = document.createElement('div');
-  divTextos.classList.add('div-textos');
-  
-  let pDescricao = document.createElement('p');
-  pDescricao.classList.add('descricao');
-  pDescricao.textContent = nome;
-  divTextos.appendChild(pDescricao);
-
-  let pPreco = document.createElement('p');
-  pPreco.classList.add('preco');
-  pPreco.textContent = preco;
-  divTextos.appendChild(pPreco);
-
-  let pCategoria = document.createElement('p');
-  pCategoria.classList.add('categoria');
-  pCategoria.textContent = categ;
-  divTextos.appendChild(pCategoria);
-
-  divProd.appendChild(divTextos);
-
-  let divIcons = document.createElement('div');
-  divIcons.classList.add('div-icons');
-
-  let iconEdit = document.createElement('i');
-  iconEdit.classList.add('fa-solid', 'fa-pen', 'edit-button');
-  divIcons.appendChild(iconEdit);
-
-  let iconDelete = document.createElement('i');
-  iconDelete.classList.add('fa-solid', 'fa-trash', 'delete-button');
-  divIcons.appendChild(iconDelete);
-
-  divProd.appendChild(divIcons);
-
-  document.getElementById('produtosList').appendChild(divProd);
 }
-
-
 
 
 
